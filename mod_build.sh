1dc770a73ea008e2d08bdad6c9e4fcce8d84abf3
yushoyamaguchi <ysh.824@outlook.jp>
2024-02-14 00:58:44 +0900

    mod_build.sh

---
+++ mod_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# コンテナを起動する
+sudo podman run --privileged -itd --name=plugsched \
+    -v /work:/work \
+    -v /work5:/work5 \
+    -v /usr/src/kernels:/usr/src/kernels \
+    -v /usr/lib/debug/lib/modules:/usr/lib/debug/lib/modules \
+    plugsched_host1:latest
+
+# コンテナ内でコマンドを実行する
+sudo podman exec -it plugsched /bin/bash -c "\
+plugsched-cli build /work5/scheduler && \
+cp /work5/scheduler/working/rpmbuild/RPMS/x86_64/scheduler-xxx-5.10.23-200.fc33.yyy.x86_64.rpm /work5/scheduler-xxx.rpm"
